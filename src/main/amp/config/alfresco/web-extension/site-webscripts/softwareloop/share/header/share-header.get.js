04cb4df97f8de826264ae0aac8cb0511fe6012fb
Paolo Predonzani <paolo.predonzani@gmail.com>
2014-09-25 17:31:09 +0200

    close #12 Add share menu item

---
+++ src/main/amp/config/alfresco/web-extension/site-webscripts/softwareloop/share/header/share-header.get.js
@@ -0,0 +1,31 @@
+<import resource="classpath:/alfresco/site-webscripts/org/alfresco/share/imports/share-header.lib.js">
+
+model.jsonModel = {
+    rootNodeId: "share-header",
+    services: getHeaderServices(),
+    widgets: [
+    {
+    id: "SHARE_VERTICAL_LAYOUT",
+    name: "alfresco/layout/VerticalWidgets",
+    config: {
+    widgets: getHeaderModel()
+    }
+    }
+    ]
+    };
+
+var inboxesMenu = {
+    id: "HEADER_INBOXES",
+    name: "alfresco/menus/AlfMenuBarItem",
+    config: {
+    id: "HEADER_INBOXES",
+    label: "Inboxes",
+    targetUrl: "hdp/ws/inboxes"
+    }
+    };
+
+var appMenu =
+widgetUtils.findObject(model.jsonModel, "id", "HEADER_APP_MENU_BAR");
+if (appMenu != null) {
+    appMenu.config.widgets.push(inboxesMenu);
+    }
